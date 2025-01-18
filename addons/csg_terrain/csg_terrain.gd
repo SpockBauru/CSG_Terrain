aa5b9eb62f908151e4cd6bcad2d9c72628de1f0c
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2025-01-18 15:36:42 -0300

    Small qol

--- addons/csg_terrain/csg_terrain.gd
+++ addons/csg_terrain/csg_terrain.gd
@@ -65,8 +65,8 @@ func _ready() -> void:
 		var path: CSGTerrainPath = CSGTerrainPath.new()
 		path.name = "Path3D"
 		var curve: Curve3D = Curve3D.new()
-		curve.add_point(Vector3(0, 0, -40))
-		curve.add_point(Vector3(0, 35, 0))
+		curve.add_point(Vector3(0, 0, 0))
+		curve.add_point(Vector3(0, 35, -40))
 		curve.set_point_in(1 , Vector3(0, 0, -15))
 		curve.set_point_out(1 , Vector3(0, 0, 15))
 		path.curve = curve
